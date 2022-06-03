44deee1af55114c12b1f8e98b8f7b0f472a15349
Craig <indigomx9@gmail.com>
2022-06-02 20:10:28 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# CRUD
+
+Initial setup of the project.  Created with JSON Server, React, Vite, Router 6, and Redux-Toolkit.
+
+
+
---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci excepturi ipsam maiores iste officiis mollitia illum, non aut, unde expedita itaque, ex voluptates cupiditate assumenda animi nam nobis. Neque, hic.</p>
+        </React.Fragment>
+    );
+};
+
+
+
